547f59a11fff40c77681d351c079ddfdbfd0beb4
neoryxyjs~ <neoryxy@gmail.com>
2023-06-24 19:23:23 -0400

    extra information new version

--- js/styles.js
+++ js/styles.js
@@ -75,18 +75,18 @@ style.appendChild(document.createTextNode(styles));
 // Agrega la etiqueta <style> al elemento <head>
 head.appendChild(style);
 
-$(document).ready(function() {
-    var myCarousel = document.getElementById('myCarousel');
-    var carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 8000, // Cambiar cada 15 segundos
-        wrap: true // Repetir al final del carrusel
-    });
+$(document).ready(function () {
+  var myCarousel = document.getElementById('myCarousel');
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 8000, // Cambiar cada 15 segundos
+    wrap: true // Repetir al final del carrusel
+  });
 });
 
 
 // Animación del footer
-$(document).ready(function() {
-    $('.footer').hide().fadeIn(1000);
+$(document).ready(function () {
+  $('.footer').hide().fadeIn(1000);
 });
 
 // Expresiones regulares para verificar el formato del correo electrónico y del RUT
@@ -94,64 +94,64 @@ const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 const rutRegex = /^\d{1,2}\.\d{3}\.\d{3}[-][0-9kK]{1}$/;
 
 // Captura el evento de envío del formulario
-$('#subscriptionForm').submit(function(event) {
-    event.preventDefault(); // Evita que se envíe el formulario automáticamente
+$('#subscriptionForm').submit(function (event) {
+  event.preventDefault(); // Evita que se envíe el formulario automáticamente
 
-    // Obtiene los valores de los campos del formulario
-    const nombre = $('#subscriptionForm input[name="nombre"]').val();
-    const apellidoPaterno = $('#subscriptionForm input[name="apellidoPaterno"]').val();
-    const apellidoMaterno = $('#subscriptionForm input[name="apellidoMaterno"]').val();
-    const rut = $('#subscriptionForm input[name="rut"]').val();
-    const fechaNacimiento = $('#subscriptionForm input[name="fechaNacimiento"]').val();
-    const correoElectronico = $('#subscriptionForm input[name="correoElectronico"]').val();
-    const comentarios = $('#subscriptionForm textarea[name="comentarios"]').val();
+  // Obtiene los valores de los campos del formulario
+  const nombre = $('#subscriptionForm input[name="nombre"]').val();
+  const apellidoPaterno = $('#subscriptionForm input[name="apellidoPaterno"]').val();
+  const apellidoMaterno = $('#subscriptionForm input[name="apellidoMaterno"]').val();
+  const rut = $('#subscriptionForm input[name="rut"]').val();
+  const fechaNacimiento = $('#subscriptionForm input[name="fechaNacimiento"]').val();
+  const correoElectronico = $('#subscriptionForm input[name="correoElectronico"]').val();
+  const comentarios = $('#subscriptionForm textarea[name="comentarios"]').val();
 
-    // Verifica el formato del correo electrónico y del RUT
-    if (!emailRegex.test(correoElectronico)) {
-        alert('El correo electrónico ingresado no es válido.');
-        return;
-    }
+  // Verifica el formato del correo electrónico y del RUT
+  if (!emailRegex.test(correoElectronico)) {
+    alert('El correo electrónico ingresado no es válido.');
+    return;
+  }
 
-    if (!rutRegex.test(rut)) {
-        alert('El número de documento (RUT) ingresado no es válido.');
-        return;
-    }
+  if (!rutRegex.test(rut)) {
+    alert('El número de documento (RUT) ingresado no es válido.');
+    return;
+  }
 
-    // Envía los datos del formulario a través de una solicitud AJAX o realiza cualquier otra acción necesaria
-    // ...
+  // Envía los datos del formulario a través de una solicitud AJAX o realiza cualquier otra acción necesaria
+  // ...
 
-    // Restablece el formulario
-    $('#subscriptionForm')[0].reset();
+  // Restablece el formulario
+  $('#subscriptionForm')[0].reset();
 
-    alert('¡Gracias por suscribirte!');
+  alert('¡Gracias por suscribirte!');
 });
 
-$(document).ready(function() {
-    // Aplicar estilos al contenedor del formulario
-    $(".form-container").css({
-      "border": "2px solid #ccc",
-      "box-shadow": "0 2px 4px rgba(0, 0, 0, 0.1)",
-      "padding": "20px"
-    });
+$(document).ready(function () {
+  // Aplicar estilos al contenedor del formulario
+  $(".form-container").css({
+    "border": "2px solid #ccc",
+    "box-shadow": "0 2px 4px rgba(0, 0, 0, 0.1)",
+    "padding": "20px"
+  });
+});
+
+
+$(document).ready(function () {
+  // Filtro por colores
+  $('#filter-color').on('change', function () {
+    var selectedColor = $(this).val();
+    $('.shoe-container').hide();
+    $('.shoe-container[data-color="' + selectedColor + '"]').show();
   });
 
-
-  $(document).ready(function() {
-    // Filtro por colores
-    $('#filter-color').on('change', function() {
-        var selectedColor = $(this).val();
-        $('.shoe-container').hide();
-        $('.shoe-container[data-color="' + selectedColor + '"]').show();
-    });
-
-    // Filtro por nombre
-    $('#filter-name').on('keyup', function() {
-        var searchTerm = $(this).val().toLowerCase();
-        $('.shoe-container').hide();
-        $('.shoe-container:contains("' + searchTerm + '")').show();
-    });
+  // Filtro por nombre
+  $('#filter-name').on('keyup', function () {
+    var searchTerm = $(this).val().toLowerCase();
+    $('.shoe-container').hide();
+    $('.shoe-container:contains("' + searchTerm + '")').show();
+  });
 });
-  
+
 
 
 
@@ -168,17 +168,17 @@ filterNameInput.addEventListener('input', filterShoes);
 
 // Función para filtrar y mostrar los contenedores de zapatillas
 function filterShoes() {
-    const selectedColor = filterColorSelect.value.toLowerCase();
-    const filterName = filterNameInput.value.toLowerCase();
+  const selectedColor = filterColorSelect.value.toLowerCase();
+  const filterName = filterNameInput.value.toLowerCase();
 
-    Array.from(shoeList.children)
-        .sort((a, b) => a.querySelector('.shoe-name').textContent.localeCompare(b.querySelector('.shoe-name').textContent))
-        .forEach(shoeContainer => {
-            const shoeColor = shoeContainer.dataset.color.toLowerCase();
-            const shoeName = shoeContainer.querySelector('.shoe-name').textContent.toLowerCase();
-            const isVisible = (selectedColor === '' || shoeColor === selectedColor) && shoeName.includes(filterName);
-            shoeContainer.style.display = isVisible ? 'block' : 'none';
-        });
+  Array.from(shoeList.children)
+    .sort((a, b) => a.querySelector('.shoe-name').textContent.localeCompare(b.querySelector('.shoe-name').textContent))
+    .forEach(shoeContainer => {
+      const shoeColor = shoeContainer.dataset.color.toLowerCase();
+      const shoeName = shoeContainer.querySelector('.shoe-name').textContent.toLowerCase();
+      const isVisible = (selectedColor === '' || shoeColor === selectedColor) && shoeName.includes(filterName);
+      shoeContainer.style.display = isVisible ? 'block' : 'none';
+    });
 }
 
 
@@ -194,4 +194,13 @@ function initMap() {
 google.maps.event.addDomListener(window, 'load', initMap);
 
 
+const sizeButtons = document.querySelectorAll('.size-button');
 
+sizeButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    sizeButtons.forEach(btn => {
+      btn.classList.remove('active');
+    });
+    button.classList.add('active');
+  });
+});
